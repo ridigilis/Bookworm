e89a7dc1e16037bac4ecd4bf872ad0d60af3d52f
rdgronii <rgroner2@gmail.com>
2023-11-20 16:15:31 -0600

    building a list with @Query

---
+++ Bookworm/EmojiRatingView.swift
@@ -0,0 +1,26 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Ricky David Groner II on 11/20/23.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int
+    
+    var body: some View {
+        switch rating {
+        case 1: Text("🤮")
+        case 2: Text("🤢")
+        case 3: Text("😐")
+        case 4: Text("😄")
+        default: Text("🤩")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)
+}
